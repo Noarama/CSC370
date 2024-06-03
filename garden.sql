f2727e1b2752b4b9a5d670eb8b6a7dd0cfc937ba
Noarama <138336255+Noarama@users.noreply.github.com>
2024-06-02 20:55:08 -0700

    Add files via upload

---
+++ garden.sql
@@ -0,0 +1,33 @@
+CREATE TABLE Crops(
+    name VARCHAR(50),
+    variety VARCHAR(100),
+    type ENUM('Vegtable', 'Fruit', 'Herb'),
+    germinationPeriod VARCHAR(120),
+    water_needs ENUM('Low', 'Moderate', 'High'),
+    fertilizationNeeds VARCHAR(120),
+    companions VARCHAR(120),
+    commonPests VARCHAR(80),
+    sunRequirements ENUM('Partial shade', 'Partial shade to full sun', 'Full sun to partial shade', 'Full sun (6-8 hours)'),
+    timeToPlant VARCHAR(120)
+);
+
+CREATE TABLE location(
+    area VARCHAR(50), 
+    province VARCHAR(120), 
+    lastFrost DATE,
+    firstFrost DATE
+); 
+
+CREATE TABLE Users(
+    isAdmin Bool, 
+    userName Varchar(50), 
+    currentlyGrows Varchar(120),
+    experienceLevel ENUM('begginer', 'intermediate', 'expert')
+);
+
+CREATE TABLE Comments(
+    CommentID int, 
+    Contents Varchar(256), 
+    userName Varchar(50), 
+    Date Date
+);
